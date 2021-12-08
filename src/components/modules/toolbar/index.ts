6cfbb292929910f2fd2c57eb0e2fbe6cf1855d7f
Алексей Овчинников <it.megasreda@gmail.com>
2021-12-08 12:52:22 +0300

    поправил отступ панели инструментов

--- src/components/modules/toolbar/index.ts
+++ src/components/modules/toolbar/index.ts
@@ -212,7 +212,7 @@ export default class Toolbar extends Module<ToolbarNodes> {
       const contentOffset = Math.floor(blockHeight / 2);
 
       this.nodes.plusButton.style.transform = `translate3d(-42px, calc(${contentOffset}px - 50%), 0)`;
-      this.Editor.Toolbox.nodes.toolbox.style.transform = `translate3d(0, calc(${contentOffset}px - 50%), 0)`;
+      this.Editor.Toolbox.nodes.toolbox.style.transform = `translate3d(10, 0, 0)`;
     } else {
       toolbarY += blockHeight;
     }
